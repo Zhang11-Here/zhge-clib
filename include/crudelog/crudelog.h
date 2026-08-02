dde964d7ad25b7cf6782b28aeaf252bdc8e71fcd
Zhang11-Here <zhang11-here@outlook.com>
2026-08-02 08:11:46 +0800

    docs: Mark colors in the comments. Correct formatting errors in the example code.

--- include/crudelog/crudelog.h
+++ include/crudelog/crudelog.h
@@ -15,16 +15,21 @@
 		fputs("\x1b[0m" "\n", ofile); \
 	} while (0)
 
-// e.g. ZHGEC_CRUDELOG_ERROR("example error: %d, %s", code, msg");
+// e.g. ZHGEC_CRUDELOG_ERROR("example %d", code);
+// output: [ERROR] [exam.c:114 func]  example 514
+// error: red
 #define ZHGEC_CRUDELOG_ERROR(...) \
 	ZHGEC_CRUDELOG_BASE("ERROR", "\x1b[31m", stderr, __VA_ARGS__)
 
+// warn: yellow
 #define ZHGEC_CRUDELOG_WARN(...) \
 	ZHGEC_CRUDELOG_BASE("WARN", "\x1b[33m", stderr, __VA_ARGS__)
 
+// info: green
 #define ZHGEC_CRUDELOG_INFO(...) \
 	ZHGEC_CRUDELOG_BASE("INFO", "\x1b[32m", stdout, __VA_ARGS__)
 
+// info: cyan
 #define ZHGEC_CRUDELOG_DEBUG(...) \
 	ZHGEC_CRUDELOG_BASE("DEBUG", "\x1b[36m", stdout, __VA_ARGS__)
 
