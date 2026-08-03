31431830ae396c254076959ea19094777852a875
Zhang11-Here <zhang11-here@outlook.com>
2026-08-03 18:53:06 +0800

    feat: Add the function of inferring structures from members. GETBASE(ptr, type, member)    ((Inspired by container_of. Thanks a lot.))

---
+++ include/getbase.h
@@ -0,0 +1,18 @@
+/* zhge-clib/include/getbase.h */
+
+#ifndef ZHGEC_GETBASE_H
+#define ZHGEC_GETBASE_H
+
+
+#include <stddef.h>
+
+
+/* e.g.
+ * struct test_struct *p = GETBASE(memp, struct test_struct, mem);
+ */
+#define GETBASE(ptr, type, member) \
+	(type *)((char *)(ptr) - offsetof(type, member))
+
+
+#endif
+
